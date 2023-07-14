d71216837eb73d973d601196956ac2fc33a89ecf
PhilipeeX <dev.philipe@outlook.com>
2023-07-14 13:37:27 -0300

    fix(Bubble Sort): fixed

--- lib/1_fase_1_nivel_2_Tarefas_8.2/3_Bubble_Sort.rb
+++ lib/1_fase_1_nivel_2_Tarefas_8.2/3_Bubble_Sort.rb
@@ -1,22 +1,20 @@
-ARRAY = [2,27,48,92,5,1,9,12,22,30,29,19,88]
-
-def bubble_sort(array)
-  j = 0
-  while j < array.size
-    maior = 0
-    i = 0
-    while i < array.length
-      if array[i] > maior
-        maior = array[i]
-      else
-        array[i - 1] = array[i]
-        array[i] = maior
+class BubbleSortAlgorithm
+  def self.bubble_sort(array)
+    j = 0
+    while j < array.size
+      maior = 0
+      i = 0
+      while i < array.length
+        if array[i] > maior
+          maior = array[i]
+        else
+          array[i - 1] = array[i]
+          array[i] = maior
+        end
+        i += 1
       end
-      i += 1
+      j += 1
     end
-    j += 1
+    p array
   end
-  p array
 end
-
-bubble_sort(ARRAY)
