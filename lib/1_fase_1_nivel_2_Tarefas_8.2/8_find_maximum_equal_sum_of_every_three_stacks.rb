c14fc59f0fac84fd034ed1d89c61af5ebf178d1f
PhilipeeX <dev.philipe@outlook.com>
2023-07-15 08:13:05 -0300

    fix(MaximumEqualSum): fixed

--- lib/1_fase_1_nivel_2_Tarefas_8.2/8_find_maximum_equal_sum_of_every_three_stacks.rb
+++ lib/1_fase_1_nivel_2_Tarefas_8.2/8_find_maximum_equal_sum_of_every_three_stacks.rb
@@ -1,21 +1,17 @@
-@stack1 = [1, 2, 1, 1]
-@stack2 = [1, 1, 3, 1]
-@stack3 = [2, 3, 1, 1]
-
-def find_maximum_equal_sum(stack1, stack2, stack3)
-  until stack1.empty? || stack2.empty? || stack3.empty?
-    if stack1.sum == stack2.sum && stack2.sum == stack3.sum
-      return stack1.sum
-    elsif stack1.sum > stack2.sum && stack1.sum > stack3.sum
-      stack1.shift
-    elsif stack2.sum > stack1.sum && stack2.sum > stack3.sum
-      stack2.shift
-    elsif stack3.sum > stack1.sum && stack3.sum > stack2.sum
-      stack3.shift
+class FindMaximumEqual
+  def self.find_maximum_equal_sum(stack1, stack2, stack3)
+    until stack1.empty? || stack2.empty? || stack3.empty?
+      if stack1.sum == stack2.sum && stack2.sum == stack3.sum
+        return stack1.sum
+      elsif stack1.sum > stack2.sum && stack1.sum > stack3.sum
+        stack1.shift
+      elsif stack2.sum > stack1.sum && stack2.sum > stack3.sum
+        stack2.shift
+      elsif stack3.sum > stack1.sum && stack3.sum > stack2.sum
+        stack3.shift
+      end
     end
+
+    0
   end
-
-  0
 end
-
-puts find_maximum_equal_sum(@stack1, @stack2, @stack3)
