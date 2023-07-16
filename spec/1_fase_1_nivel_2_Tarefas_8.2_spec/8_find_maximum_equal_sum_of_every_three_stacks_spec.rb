ba12ccfa3756dd4c23b26de8f5ecc5aaaa9c0ced
PhilipeeX <dev.philipe@outlook.com>
2023-07-16 15:09:11 -0300

    test(FindMaximumSum): tests created

--- spec/1_fase_1_nivel_2_Tarefas_8.2_spec/8_find_maximum_equal_sum_of_every_three_stacks_spec.rb
+++ spec/1_fase_1_nivel_2_Tarefas_8.2_spec/8_find_maximum_equal_sum_of_every_three_stacks_spec.rb
@@ -15,22 +15,12 @@ RSpec.describe FindMaximumEqual do
 
     it 'returns the maximum equal sum when sums are already equal' do
       stack1 = [3, 5, 2]
-      stack2 = [4, 4, 4]
+      stack2 = [4, 4, 2]
       stack3 = [1, 3, 6]
 
       result = FindMaximumEqual.find_maximum_equal_sum(stack1, stack2, stack3)
 
-      expect(result).to eq(15)
-    end
-
-    it 'returns the maximum equal sum after removing elements' do
-      stack1 = [1, 2, 3, 4]
-      stack2 = [5, 6, 7, 8]
-      stack3 = [9, 10, 11, 12]
-
-      result = FindMaximumEqual.find_maximum_equal_sum(stack1, stack2, stack3)
-
-      expect(result).to eq(27)
+      expect(result).to eq(10)
     end
 
     it 'returns 0 when it is not possible to have equal sums' do
