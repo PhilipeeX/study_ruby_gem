24f368a0b3dcd1a2fa23768e917cc92502a6043b
Philipe <dev.philipe@outlook.com>
2023-07-17 10:30:28 -0300

    fix(CountCharacters): class created

--- lib/1_fase_1_nivel_2_Tarefas_8.2/10_count_uppercase_lowercase_special_number.rb
+++ lib/1_fase_1_nivel_2_Tarefas_8.2/10_count_uppercase_lowercase_special_number.rb
@@ -1,23 +1,25 @@
-def count(string)
-  uppercase = 0
-  lowercase = 0
-  special = 0
-  number = 0
+class CountUppercaseLowercaseEtc
+  def self.count(string)
+    uppercase = 0
+    lowercase = 0
+    special = 0
+    number = 0
 
-  string.gsub(' ', '').split('').each do |character|
-    case character
-    when /[a-z]/
-      lowercase += 1
-    when /[A-Z]/
-      uppercase += 1
-    when /\W/
-      special += 1
-    when /\d/
-      number += 1
+    string.gsub(' ', '').split('').each do |character|
+      case character
+      when /[a-z]/
+        lowercase += 1
+      when /[A-Z]/
+        uppercase += 1
+      when /\W/
+        special += 1
+      when /\d/
+        number += 1
+      end
     end
+    puts "Upper case letters : #{uppercase}"
+    puts "Lower case letters : #{lowercase}"
+    puts "Numbers : #{number}"
+    puts "Special Characters : #{special}"
   end
-  puts "Upper case letters : #{uppercase}"
-  puts "Lower case letters : #{lowercase}"
-  puts "Numbers : #{number}"
-  puts "Special Characters : #{special}"
 end
