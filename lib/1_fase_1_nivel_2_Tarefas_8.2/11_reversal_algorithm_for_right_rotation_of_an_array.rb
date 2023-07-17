1f012f62e1ead55c11405df4efcb54690e286486
Philipe <dev.philipe@outlook.com>
2023-07-17 10:31:08 -0300

    fix(ReversalAlgorithm): class created and logic fixed

--- lib/1_fase_1_nivel_2_Tarefas_8.2/11_reversal_algorithm_for_right_rotation_of_an_array.rb
+++ lib/1_fase_1_nivel_2_Tarefas_8.2/11_reversal_algorithm_for_right_rotation_of_an_array.rb
@@ -1,6 +1,26 @@
-def rotate_array(array, k)
-  k.times do
-    array.unshift(array.pop)
+class RotateArray
+  def self.rotate_array(array, k)
+    n = array.length
+
+    return array if n.zero?
+
+    k = k % n
+
+    reverse_array(array, 0, n - 1)
+    reverse_array(array, 0, k - 1)
+    reverse_array(array, k, n - 1)
+
+    array
   end
-  array.join(' ')
+
+  def self.reverse_array(array, start_index, end_index)
+    while start_index < end_index
+      temp = array[start_index]
+      array[start_index] = array[end_index]
+      array[end_index] = temp
+
+      start_index += 1
+      end_index -= 1
+    end
+  end
 end
